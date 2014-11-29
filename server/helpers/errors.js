d0bbc71c32bf4cdfa955222bdf64199190de5969
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-30 01:30:57 +0500

    added error lists.

---
+++ server/helpers/errors.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Umayr on 11/29/2014.
+ */
+
+var errors = {};
+
+errors.invalid = {
+    err: "invalid request",
+    msg: "you mad, bro?"
+}
+errors.wrong = {
+    err : "something went wrong",
+    msg : "someone is on drugs. >.>"
+}
+module.exports = errors;
