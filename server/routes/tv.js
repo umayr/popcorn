403575bb72e093c7409c0766ff4fb5e938c1fad8
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-30 01:30:35 +0500

    seperated tv related routes

---
+++ server/routes/tv.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Umayr on 11/29/2014.
+ */
+var express = require('express');
+var router = express.Router();
+var tvShowFactory = require('../factories/tvShow');
+var is = require('is');
+var errors = require('../helpers/errors');
+
+router.get('/get/:id', function (req, res) {
+    var id = parseInt(req.params.id);
+    if (is.int(id)) {
+        tvShowFactory.getByID(id).then(function (result) {
+            res.json(result);
+        }).catch(function (error) {
+            res.json(error);
+        });
+    }
+    else {
+        res.json(errors.invalid);
+    }
+});
+
+router.get('/search/', function (req, res) {
+    var raw = req.param("name");
+    var decoded = decodeURIComponent(raw);
+
+    res.json({
+        raw: raw,
+        decoded: decoded
+    });
+});
+
+module.exports = router;
