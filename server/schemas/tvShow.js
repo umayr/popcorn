1e0b4e42a4f210230b2316b0dd7d0e29d6a7e316
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-30 01:26:14 +0500

    added additional fields to cover more props

--- server/schemas/tvShow.js
+++ server/schemas/tvShow.js
@@ -5,13 +5,17 @@ var mongoose = require('mongoose');
 var tvShow = new mongoose.Schema({
     id: Number,
     backdrop_path: String,
-    created_by: {
-        id: Number,
-        name: String,
-        profile_path: String
-    },
+    created_by: [
+        {
+            id: Number,
+            name: String,
+            profile_path: String
+        }
+    ],
     episode_run_time: [Number],
-    networks: [String],
+    networks: [
+        {id: Number, name: String}
+    ],
     first_air_date: Date,
     last_air_date: Date,
     genres: [
@@ -19,12 +23,78 @@ var tvShow = new mongoose.Schema({
     ],
     homepage: String,
     in_production: Boolean,
-    language: [String],
+    languages: [String],
     name: String,
+    _name: String,
     original_name: String,
-    original_country: [String],
+    origin_country: [String],
     number_of_episodes: Number,
     number_of_seasons: Number,
+    content_ratings: [
+        {
+            iso_3166_1: String,
+            rating: String
+        }
+    ],
+    credits: {
+        cast: [
+            {
+                character: String,
+                credit_id: String,
+                id: Number,
+                name: String,
+                profile_path: String,
+                order: Number
+            }
+        ],
+        crew: [
+            {
+                department: String,
+                id: Number,
+                name: String,
+                job: String,
+                profile_path: String
+            }
+        ]},
+    external_ids: {
+        imdb_id: String,
+        freebase_id: String,
+        freebase_mid: String,
+        id: Number,
+        tvdb_id: Number,
+        tvrage_id: Number
+    },
+    images: {
+        backdrops: [
+            {
+                aspect_ratio: Number,
+                file_path: String,
+                height: Number,
+                iso_639_1: String,
+                vote_average: Number,
+                vote_count: Number,
+                width: Number
+            }
+        ],
+        id: Number,
+        posters: [
+            {
+                aspect_ratio: Number,
+                file_path: String,
+                height: Number,
+                iso_639_1: String,
+                vote_average: Number,
+                vote_count: Number,
+                width: Number
+            }
+        ]
+    },
+    keywords: [
+        {
+            id: Number,
+            name: String
+        }
+    ],
     overview: String,
     popularity: Number,
     poster_path: String,
@@ -40,6 +110,17 @@ var tvShow = new mongoose.Schema({
         }
     ],
     status: String,
+    videos: [
+        {
+            id: String,
+            iso_639_1: String,
+            key: String,
+            name: String,
+            site: String,
+            size: Number,
+            type: String
+        }
+    ],
     vote_average: Number,
     vote_count: Number
 });
